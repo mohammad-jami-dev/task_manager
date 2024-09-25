3ae188a921ebb4ee70d31752b1dcbfc9e9711b3a
Mohammad Jami <anssjami@gmail.com>
2024-09-25 15:39:13 +0600

    Initial commit

---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Text('data'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
